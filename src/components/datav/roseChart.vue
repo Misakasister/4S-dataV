cd2e90ac1bb02cc07d163697f1f9e18bc4afc41f
misakasister <y59455320@163.com>
2020-12-31 22:17:22 +0800

    ajax请求改为同步

---
+++ src/components/datav/roseChart.vue
@@ -0,0 +1,115 @@
+<template>
+  <div id="rose-chart">
+    <div class="rose-chart-title">销售员销售额分布</div>
+    <dv-charts :option="option" />
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+export default {
+  name: 'RoseChart',
+  data () {
+    return {
+      option: {}
+    }
+  },
+  methods: {
+    createData () {
+      const { randomExtend } = this
+       let that = this
+      this.option = {
+        series: [
+          {
+            type: 'pie',
+            radius: '50%',
+            roseSort: false,
+             data: [
+            ],
+            insideLabel: {
+              show: false
+            },
+            outsideLabel: {
+              formatter: '{name} {percent}%',
+              labelLineEndLength: 20,
+              style: {
+                fill: '#fff'
+              },
+              labelLineStyle: {
+                stroke: '#fff'
+              }
+            },
+            roseType: true
+          }
+        ],
+        color: ['#da2f00', '#fa3600', '#ff4411', '#ff724c', '#541200', '#801b00', '#a02200', '#5d1400', '#b72700']
+      }
+
+      $.ajax({
+    url:this.apiUrl.sellCar,
+    type:'POST', //GET
+    async:false,    //或false,是否异步
+    data:{
+    },
+    timeout:5000,    //超时时间
+    dataType:'json',    //返回的数据格式：
+    beforeSend:function(xhr){
+    },
+    success:function(data,textStatus,jqXHR){
+          for(let i = 0; i < data.length; i++){
+         let obj={
+           name:data[i].sell_name,
+           value: data[i].price,
+         }
+         that.option.series[0].data.push(obj);
+         }
+
+    },
+    error:function(xhr,textStatus){
+    },
+    complete:function(){
+    }
+})
+    },
+    randomExtend (minNum, maxNum) {
+      if (arguments.length === 1) {
+        return parseInt(Math.random() * minNum + 1, 10)
+      } else {
+        return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
+      }
+    }
+  },
+  mounted () {
+
+
+    const { createData } = this
+
+
+    setInterval(createData, 300000)
+    createData()
+  },
+}
+</script>
+
+<style lang="less">
+#rose-chart {
+  width: 30%;
+  height: 100%;
+  background-color: rgba(6, 30, 93, 0.5);
+  border-top: 2px solid rgba(1, 153, 209, .5);
+  box-sizing: border-box;
+
+  .rose-chart-title {
+    height: 50px;
+    font-weight: bold;
+    text-indent: 20px;
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+  }
+
+  .dv-charts-container {
+    height: calc(~"100% - 50px");
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import './assets/common.less'
+
+import dataV from '@jiaminghi/data-view'
+import axios from 'axios'
+Vue.config.productionTip = false
+axios.defaults.withCredentials = true
+Vue.prototype.axios = axios
+Vue.use(dataV)
+axios.defaults.withCredentials=true,
+Vue.prototype.apiUrl = {
+  sellCar: 'http://192.168.1.102:8080/api/SellCar'
+}
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
